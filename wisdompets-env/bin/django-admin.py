21b333c5e43b3695fc0ade4b87df89b6cd74a9d5
Ufuk ORHAN <ufuk.orhan@logsign.net>
2022-07-19 20:05:26 +0300

    feat: add django workshop project

---
+++ wisdompets-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ufukorhan/Desktop/wisdompets-project/wisdompets-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
